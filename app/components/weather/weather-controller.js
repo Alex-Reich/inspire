5fff9b57c875313f39da220873659c1fea9c599a
Alex Reich <atrain.reich@gmail.com>
2018-05-14 14:45:18 -0600

    Style tweaks

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -6,7 +6,7 @@ function WeatherController(){
 		console.log(weather);
 		var tempF = Math.floor((9/5)*(weather.main.temp)-459.67)
 		var template =`
-		<h2>It is currently ${tempF}° in ${weather.name}</h2>
+		<h2>It is currently ${tempF}°F in ${weather.name}</h2>
 		`
 		document.getElementById("weather").innerHTML = template
 		//What can you do with this weather object?
